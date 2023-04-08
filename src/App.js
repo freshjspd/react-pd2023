6499f82e4b84b357ee4dc0824e39c90ab9284ff0
freshjspd <freshjs.pd@gmail.com>
2023-04-08 11:16:24 +0300

    add state user

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import './App.css';
 import Greeting from './Components/Greeting';
+import React, { Component } from 'react'
 
 const currentUser = {
   firstName: 'John',
   lastName: 'Adson',
-  email: 'john7@mail.com'
+  email: 'john7@mail.com',
+  role: 'moderator'
 };
 
-function App() {
-  return (
-    <>
-      <Greeting user={currentUser} isOnline={true}/>
-    </>
-  );
+export default class App extends Component {
+  constructor(props){ 
+    super(props); 
+    this.state = {
+      user: currentUser,
+    };
+  }
+  render() {
+    const {user} = this.state;
+    return (
+      <Greeting user={user}/>
+    );
+  }
 }
 
-export default App;
+
--- src/Components/Greeting/index.jsx
+++ src/Components/Greeting/index.jsx
@@ -4,13 +4,10 @@ import styles from './Greeting.module.css';
 export class Greeting extends Component {
   constructor(props){ super(props); }
   render() {
-    const {firstName, lastName, email} = this.props.user;
-    const {isOnline} = this.props;
+    const {firstName, lastName} = this.props.user;
     return (
         <div className={styles.card}>
             <h1 className={styles.hello}>Hello, {firstName} {lastName} !!!</h1>
-            <p>my email: {email}</p>
-            <p className={styles.net}>online: {isOnline ? "yes" : "no"}</p>
         </div>
     )
   }
