c97fc2447264d48616b4e980678d457d529701e3
freshjspd <freshjs.pd@gmail.com>
2023-04-08 10:18:28 +0300

    greeting class component with easy props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Greeting from './Components/Greeting';
 function App() {
   return (
     <>
-      <Greeting />
+      <Greeting firstName={"John"} lastName={"Smith"}/>
+      <Greeting firstName={"Kate"} lastName={"Fox"}/>
     </>
   );
 }
--- src/Components/Greeting/index.jsx
+++ src/Components/Greeting/index.jsx
@@ -1,17 +1,16 @@
 import React, { Component } from 'react'
 
 export class Greeting extends Component {
+  constructor(props){ super(props); }
   render() {
+    const {firstName, lastName} = this.props;
     return (
         <>
-            <h1>My first app</h1>
-            <p>Hello, React!!!</p>
-            <p>08-04-2023</p>
+            <h1>Hello, {firstName} {lastName} !!!</h1>
         </>
     )
   }
 }
-
 export default Greeting;
 /*
 import React from 'react'
