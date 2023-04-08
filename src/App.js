877a5a26079d6a2951c9ae05e832cd9cdcb644ec
freshjspd <freshjs.pd@gmail.com>
2023-04-08 13:45:58 +0300

    component product card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
 import React, { Component } from 'react';
-import Counter from './Components/Counter';
+import ProductCard from './Components/ProductCard';
+
+const vini = {
+  imgSrc: 'https://static.wikia.nocookie.net/winniepooh/images/2/29/%D0%92%D0%B8%D0%BD%D0%BD%D0%B8-%D0%9F%D1%83%D1%85.png/revision/latest?cb=20210129021723&path-prefix=ru',
+  name: 'Вини Пух',
+  price: 500, 
+};
 
 export default class App extends Component {
   constructor(props){ 
@@ -9,8 +15,7 @@ export default class App extends Component {
   render() {
     return (
       <>
-        <Counter step={5} />
-        <Counter step={100} />
+        <ProductCard product={vini}/>
       </>
     );
   }
---
+++ src/Components/ProductCard/index.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import styles from './productCard.module.css';
+
+export class ProductCard extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        isSelected: false,
+    };
+  }
+  select = () => {
+    const {isSelected} = this.state;
+    this.setState({isSelected: !isSelected});
+  }
+  render() {
+    const {imgSrc, name, price} = this.props.product;
+    const selectedStyle = this.state.isSelected ? styles.selectedCard : null;
+    return (
+        <div className={[styles.card, selectedStyle]} onClick={this.select}>
+            <div className={styles.imgContainer}>
+                <img src={imgSrc} alt='product'/>
+                <p>{name}</p>
+                <p>{price}</p>
+            </div>
+        </div>
+    )
+  }
+}
+
+export default ProductCard;
