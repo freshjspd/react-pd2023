ae83fb9102b1d6f49eecfea2f06f3bf9517598b9
freshjspd <freshjs.pd@gmail.com>
2023-04-08 10:07:54 +0300

    greeting class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import Greeting from './Components/Greeting';
 
 function App() {
   return (
     <>
-      <h1>My first app</h1>
-      <p>Hello, React!!!</p>
-      <p>08-04-2023</p>
+      <Greeting />
     </>
   );
 }
