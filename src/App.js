6c4f9b876edae1ffa922567c92f5ddb42fcab458
freshjspd <freshjs.pd@gmail.com>
2023-04-08 10:49:29 +0300

    hello jsx

--- src/App.js
+++ src/App.js
@@ -7,10 +7,19 @@ const currentUser = {
   email: 'john7@mail.com'
 };
 
+let k = 10;
+
 function App() {
   return (
     <>
       <Greeting user={currentUser} isOnline={true}/>
+      <p>Hello, React!!!</p>
+      <p>1+1</p>
+      <p>{1+1}</p>
+      <p>{k}</p>
+      <p>{++k}</p>
+      <p id={++k}>text</p>
+      <p id={10-4}></p>
     </>
   );
 }
