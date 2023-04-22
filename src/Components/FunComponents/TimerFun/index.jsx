46f3b9e60452c74f76eb4a85a2686cc5db5630e8
freshjspd <freshjs.pd@gmail.com>
2023-04-22 11:40:40 +0300

    hook useEffect: timer

--- src/Components/FunComponents/TimerFun/index.jsx
+++ src/Components/FunComponents/TimerFun/index.jsx
@@ -1,9 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function TimerFun(props) {
   const [count, setCount] = useState(new Date(0,0,0,0,0,0,0));
   const [isRun, setIsRun] = useState(false);
-  let id = null;
+
+  const reset = () => {
+    setCount(new Date(0,0,0,0,0,0,0));
+  }
+
+  useEffect (()=>{
+    if(isRun) {
+        const id = setInterval(go, 1000);
+        return () => {clearInterval(id);}
+    }
+  },[count, isRun]);
 
   const go = () => {
     const countNew = new Date(count.valueOf());
@@ -12,25 +22,11 @@ export default function TimerFun(props) {
     setIsRun(true);
   }
 
-  const start = () => {
-    setIsRun(true);
-    if(isRun) { id = setInterval(go, 1000);}
-  }
-
-  const stop = () => {
-    clearInterval(id);
-    id = null;
-  }
-
-  const reset = () => {
-    setCount(new Date(0,0,0,0,0,0,0));
-  }
-
     return (
       <>
         <p>{count.toLocaleTimeString('en-GB')}</p>
-        <button onClick={start}>start</button>
-        <button onClick={stop}>stop</button>
+        <button onClick={()=> setIsRun(true)}>start</button>
+        <button onClick={()=> setIsRun(false)}>stop</button>
         <button onClick={reset}>reset</button>
       </>
     )
