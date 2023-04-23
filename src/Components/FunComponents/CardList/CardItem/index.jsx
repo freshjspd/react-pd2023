a1b74b1b5300dd346b4df6b86f9957534aef8472
freshjspd <freshjs.pd@gmail.com>
2023-04-23 13:31:44 +0300

    react icons lib

--- src/Components/FunComponents/CardList/CardItem/index.jsx
+++ src/Components/FunComponents/CardList/CardItem/index.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import styles from './CardItem.module.css';
+import Icon from '@mdi/react';
+import {mdiAccountOutline, mdiAccountCheck} from '@mdi/js';
 
 export default function CardItem(props) {
   const {user:{fullName, isSelected}, select} = props;
 
    const selectedStyle = isSelected ? styles.selectedCard : null;
+   const userIcon = isSelected ?  mdiAccountCheck : mdiAccountOutline;
 
    return (
     <li onClick={select} className={selectedStyle}>
-        <p>User: {fullName}</p>
+        <Icon path={userIcon} size={1.5} color="blue"/>
+        <span>{fullName}</span>
     </li>
 );
 }
