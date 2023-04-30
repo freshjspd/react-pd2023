68d415ec6ef4240e686d506cb2d47a73b9e47564
freshjspd <freshjs.pd@gmail.com>
2023-04-30 13:04:08 +0300

    UsersLoad: pagination

--- src/Components/UsersLoad/index.jsx
+++ src/Components/UsersLoad/index.jsx
@@ -10,10 +10,18 @@ export default function UsersLoad(props) {
   const [users, setUsers] = useState([]);
   const [isFetching, setIsFetching] = useState(false); //isLoading
   const [isError, setIsError] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const gotoPrev = () => {
+    if(currentPage > 1) {setCurrentPage(currentPage - 1); }
+  }
+  const gotoNext = () => {
+    setCurrentPage(currentPage + 1);
+  }
   
   async function load({results, page, seed}){
     setIsFetching(true);
-    await fetch(`https://randomuser.me/api/?results=${results}&seed=${seed}&page=${page}`)
+    await fetch(`https://randomuser.me/api/?results=${results}&seed={seed}&page=${page}`)
     .then((response) => {
       //console.log(response);
       if(!response.ok) {throw new Error(response.statusText);}
@@ -26,12 +34,15 @@ export default function UsersLoad(props) {
 
   useEffect(() => {
     load(options);
-  }, [] );
+  }, [currentPage] );
 
   return (
     <>
     {isError && <h3>Error load</h3>}
     {isFetching && <h3>Loading...</h3>}
+    <button onClick={gotoPrev}>prev page</button>
+    <button onClick={gotoNext}>next page </button>
+    <span>Page: {currentPage}</span>
     <ol>
         {users.map((u,i) => <li key={i}>{JSON.stringify(u)}</li>)}
     </ol>
