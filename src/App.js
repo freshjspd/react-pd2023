0a1952ee901b08cd5748c16623f1b1459610a2e9
freshjspd <freshjs.pd@gmail.com>
2023-04-30 11:41:38 +0300

    load data with fetch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import './App.css';
 import React, { Component } from 'react';
-import DivRender from './Components/FunComponents/DivRender';
-import CatMouseTracker from './Components/FunComponents/CatMouseTracker';
 
-export default class App extends Component {
-   render() {
-    return (
-      <>
-        <DivRender />
-        <CatMouseTracker />
-      </>
-    );
-  }
+const options = {
+  results: 10,
+  page: 1,
+  seed: 'pd2023'
 }
+
+function load({results, page, seed}){
+  fetch(`https://randomuser.me/api/?results=${results}&seed=${seed}&page=${page}`)
+  .then((response) => response.json())
+  .then((data) => console.log(data.results))
+  .catch((error) => console.log(error));
+}
+
+function App(){
+  load(options);
+  return(
+    <>
+      <h2>Load users</h2>
+    </>
+  );
+}
+
+export default App;
 
 
